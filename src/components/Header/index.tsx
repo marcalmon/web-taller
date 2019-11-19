7aa872ef4154b366eba569acc27a9e64c945d308
Mariana <mariana.calmon@wolox.com.ar>
2019-11-19 16:36:12 -0300

    Header component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
 import React from 'react';
 
 import styles from './styles.module.scss';
+import logo from '../../assets/logo.svg';
+
+console.log(styles.navContainer)
+
+function Header() {
+  return (
+    <nav className={styles['nav-container']}>
+      <img src={logo} className={styles['nav-img']} />
+      <ul className={styles['nav-list']}>
+        <li>HOME</li>
+        <li>NOSOTROS</li>
+        <li>SERVICIOS</li>
+        <li>GALERIA</li>
+        <li>CONTACTO</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header;
