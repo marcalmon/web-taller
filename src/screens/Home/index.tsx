c77e79cf0b805573512431bddcac053ac42acf94
Mar Calmon <marical2222@gmail.com>
2019-11-25 10:29:37 -0300

    Armed Services section.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
-import './styles.module.scss';
+import styles from './styles.module.scss';
 import Header from '../../components/Header';
 import ImageHome from '../../components/ImageHome';
 import ServicesSquare from '../../components/ServicesSquare';
 
 import pintura from '../../assets/spray-gun.svg';
+import autopartes from '../../assets/lug.svg';
+import mecanica from '../../assets/tools.svg';
 
 const Home: React.FC = () => {
   return (
-    <div className="Home">
+    <div className={styles['home']}>
       <Header />
       <ImageHome />
-      <ServicesSquare icon={pintura} />
+      <h1>NOSOTROS</h1>
+      <div className={styles['home-services']}>
+        <h1>SERVICIOS</h1>
+        <ServicesSquare icon={pintura} text="CHAPA Y PINTURA" />
+        <ServicesSquare icon={autopartes} text="VENTA DE AUTOPARTES RESTAURADAS" />
+        <ServicesSquare icon={mecanica} text="MECANICA INTEGRAL" />  
+      </div>
     </div>
   );
 }
